759e9c239d8af4816c47cdca8cf8dc83a203f7f2
Ryuji Oda <ryuji.oda@gmail.com>
2021-03-19 08:44:54 +0900

    add: adapter and set data from SleepNight class

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,4 +1,26 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
-class SleepNightAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    val data = listOf<SleepNight>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * RecyclerView needs to know how many items the adapter has for it to display,
+     * and it does that by calling getItemCount()
+     * */
+    override fun getItemCount(): Int {
+        return data.size
+    }
 }
